0f793c10bb5ae5247f521bb1dd4e48c2e7eead55
Hasibul Hasan Hasib <mdhasibulctg100@gmail.com>
2022-12-04 10:45:11 +0600

    boking added

--- index.js
+++ index.js
@@ -86,15 +86,42 @@ app.get('/posts', async (req, res) => {
     });
 })
 app.get('/users', async (req, res) => {
-    db.query("SELECT * FROM users", (err, result) => {
+    db.query("SELECT * FROM user", (err, result) => {
         err ? console.log(err) : res.send(result);
     });
 })
+
 app.get('/bookings', async (req, res) => {
     db.query("SELECT * FROM booking", (err, result) => {
         err ? console.log(err) : res.send(result);
     });
 })
+
+app.delete('/bookings', async (req, res) => {
+    const { booking_id } = req.body;
+    console.log(booking_id);
+    db.query(`DELETE FROM booking WHERE 'booking_id'='${booking_id}'`, (err, result) => {
+        err ? console.log(err) : res.send(result);
+    });
+})
+
+app.post('/bookings', async (req, res) => {
+    const { user_id, vendor_id, post_id, status } = req.body;
+    console.log(req.body)
+    db.query(`INSERT INTO booking (user_id, vendor_id, post_id, booking_status) VALUES ('${user_id}', '${vendor_id}', '${post_id}', '${status}')`, (err, result) => {
+        err ? console.log(err) : res.send(result);;
+    });
+})
+
+
+app.post('/reviews', async (req, res) => {
+    const { user_id, rating, review_com, postId, status } = req.body;
+    console.log(req.body)
+    db.query(`INSERT INTO review (user_id, review_rating, review_com, booking_status, post_id) VALUES ('${user_id}', '${rating}', '${review_com}', '${status}', '${postId}')`, (err, result) => {
+        err ? console.log(err) : res.send(result);
+    });
+})
+
 app.get('/reviews', async (req, res) => {
     db.query("SELECT * FROM review", (err, result) => {
         err ? console.log(err) : res.send(result);
